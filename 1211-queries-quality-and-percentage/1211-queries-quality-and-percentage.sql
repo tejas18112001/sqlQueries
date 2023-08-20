f3d31c20494f15317a338d37ec0d2871485f49a5
tejas18112001 <89287728+tejas18112001@users.noreply.github.com>
2023-08-20 16:13:41 +0530

    Time: 1239 ms (81.67%), Space: 0B (100.00%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -2,8 +2,8 @@
 
 
 select query_name ,
-  round(avg(rating / position) , 2) as quality ,
-  round(avg(rating < 3)*100 ,2) as poor_query_percentage
+  round(sum(rating / position) / count(query_name) , 2) as quality ,
+  round(sum(case when rating < 3 then 1 else 0 end)/ count(query_name)*100 ,2) as poor_query_percentage
 from Queries
 group by query_name ;
 
