023c50319942679eb4e14d4c77c9175c042cd2ca
tejas18112001 <89287728+tejas18112001@users.noreply.github.com>
2023-08-22 18:37:18 +0530

    Time: 2 ms (49.78%), Space: 6 MB (37.93%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
-    string convertToTitle(int columnNumber) {
+    string convertToTitle(int k) {
          
-        map<int  , char>mp ;
-        
-        for(int i = 0, j = 1 ; i< 26 ; i++ , j++) {
-            mp[j] = 'A'+  i ;
+         map<int  , char>mp ;
+         int j = 1 ;
+        for(char i = 'A' ; i <= 'Z' ; i++) {
+            mp[j] = i ;
+            j++ ;
         }
         
-        /*
-        39 / 26 -> 1 
-        39 % 26 = 13
-        */
+     
+        
         string ans = "" ;
-        int k=columnNumber;
-        while(k){
-           ans = mp[k%26? k%26:26] + ans;
-           // cout<<k<<" ";
-            if(k%26==0){
-                k = k/26 -1;
-            }
-            else k/=26;
+        
+        while(k) {
+            ans = mp[k%26? k%26 : 26] + ans ;
+            if(k%26 == 0) 
+                k = k/26 -1 ;
+            else
+                k =k/26 ;
         }
         
-        // cout<<ans<<endl;
-        // ans += mp[1200%26] ;
-        
-    
-        return ans;
+        return ans ;
+        /*
+               52%26 = 0
+               52 /26 - > 2
+               52 % 26 = 0
+        */
         
     }
 };
